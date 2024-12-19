ca111ccec851fc462ddc9c64f69cd8283c7784a4
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2024-12-19 19:20:23 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,22 +1,23 @@
 N, K = map(int, input().split())
 arr = []
-temp = None
-rank = []
-for i in range(N):
-    L = list(map(int, input().split()))
-    arr.append(L)
 
-arr.sort(key = lambda x :(x[1], x[2], x[3]), reverse=True) # 순위 졍렬
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
 
-for row in arr:
+arr.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
+
+
+rank = [0] * (N + 1) 
+current_rank = 1      
+prev_score = None     
+
+for i, row in enumerate(arr):
     score = (row[1], row[2], row[3])
-    if temp == score:
-        rank[-1].append(row[0])
-    else:
-        rank.append([row[0]])
-        temp = score
+    if score == prev_score:  
+        rank[row[0]] = current_rank
+    else:  
+        current_rank = i + 1
+        rank[row[0]] = current_rank
+    prev_score = score
 
-for i, group in enumerate(rank):
-    if K in group:
-        print(i+1)
-        break
+print(rank[K])
