34887807d8f27af0cb92547f7a67221e2fbf6a2b
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2024-12-31 01:47:56 +0900

    [Bronze II] Title: 모르고리즘 회장님 추천 받습니다, Time: 2688 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.py
+++ 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.py
@@ -1,8 +1,11 @@
 N = int(input())
-info = {}
+info = {'A' : 0} #기본 값
 for _ in range(N):
     A = input().split()
-    info[A[0]] = int(A[1])
+    if max(info.values()) <= int(A[1]):
+        info[A[0]] = int(A[1])
+
+info.pop('A')
 
 sort = sorted(info.items(), key=lambda x: (-x[1], x[0]))
 print(sort[0][0])
