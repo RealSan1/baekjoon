775013cccfe0805a3940243b6a1a7bc5ff9c20af
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-01-31 22:43:41 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
@@ -1,23 +1,5 @@
 import sys
 input = sys.stdin.readline
 
-T = 1000001
-prime = [True] * T
-prime[0], prime[1] = False, False
-
-for i in range(2, int(T ** 0.5)+1):
-    for j in range(i*i, T, i):
-        if prime[j]:
-            prime[j] = False
-
-A = []
-for i in range(len(prime)):
-    if not prime[i]:
-        A.append(i)
-
 N = int(input())
-
-for i in A:
-    if i > N:
-        print(i)
-        break
+print(1000000000)
