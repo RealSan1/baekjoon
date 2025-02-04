64cd2bc0eb6c2394ba2fdd5f06a90af118207f7e
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-02-05 01:01:34 +0900

    [Silver II] Title: K번째 소수, Time: 7972 ms, Memory: 111460 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,7 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-T = 7368788
+K = int(input())
+
+T = K * 15
 prime = [True] * T
 prime[0], prime[1] = False, False
 for i in range(2, int(T ** 0.5)+1):
@@ -14,6 +16,10 @@ for i in range(len(prime)):
     if prime[i]:
         A.append(i)
 
-K = int(input())-1
-
-print(A[K])
+count = 0
+for i in range(T):
+    if prime[i]:
+        count += 1
+        if count == K:
+            print(i)
+            break
