3f3629a1a7c77f7db43ba64a9c49b823fbf7bedc
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-02-06 17:01:58 +0900

    [Silver V] Title: 귀찮음, Time: 296 ms, Memory: 63380 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.py
+++ 백준/Silver/16208. 귀찮음/귀찮음.py
@@ -6,8 +6,9 @@ arr = list(map(int, input().split()))
 result = 0
 
 arr.sort()
-for i in range(len(arr)-1):
-    for j in range(i+1, len(arr)):
-        result += arr[i] * arr[j]
+hap = sum(arr)
+for i in range(N-1):
+    hap -= arr[i]
+    result += arr[i] * hap
 
 print(result)
