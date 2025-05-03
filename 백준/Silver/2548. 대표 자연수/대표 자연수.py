0a5c635a018c596d0b85349c8e34044809a95663
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-05-04 01:51:52 +0900

    [Silver III] Title: 대표 자연수, Time: 40 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+
+median = A[N // 2] if N % 2 == 1 else A[N // 2 - 1]
+
+result = sum(abs(a - median) for a in A)
+print(median)
