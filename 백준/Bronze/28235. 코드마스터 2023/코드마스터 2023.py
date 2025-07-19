2b4e12ddaecd3af31360526ef67189fa501cfca9
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-07-20 00:29:21 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+  
+num = input().strip()
+if num == "SONGDO":
+    print("HIGHSCHOOL")
+elif num == "CODE":
+    print("MASTER")
+elif num == "2023":
+    print("0611")
+elif num == "ALGORITHM":
+    print("CONTEST")
