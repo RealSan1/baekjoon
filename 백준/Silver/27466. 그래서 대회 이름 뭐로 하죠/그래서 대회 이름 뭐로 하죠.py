c5071560af14122f40e71da4c139dd77505cbdab
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-09-07 02:05:00 +0900

    [Silver IV] Title: 그래서 대회 이름 뭐로 하죠, Time: 96 ms, Memory: 110340 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.py
+++ 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.py
@@ -1,15 +1,24 @@
-import sys, itertools
+import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
-T = input()
-T = sorted(T, reverse=True)
+S = input()
+
 V = ['A', 'E', 'I', 'O', 'U']
-if T.count('A') < 2:
+arr = []
+
+for i in range(N-1, 0, -1):
+    if len(arr) == 0 and S[i] not in V:
+        arr.append(S[i])
+    elif (len(arr) == 1 or len(arr) == 2) and S[i] == 'A':
+        arr.append(S[i])
+    elif len(arr) >= 3:
+        arr.append(S[i])
+
+    if len(arr) == M:
+        break
+
+if len(arr) != M:
     print('NO')
 else:
-    for i in itertools.permutations(T, M):
-        i = ''.join(i)
-        if i[-2] == 'A' and i[-3] == 'A' and i[-1] not in V:
-            print(i)
-            break
+    print(''.join(arr[::-1]))
