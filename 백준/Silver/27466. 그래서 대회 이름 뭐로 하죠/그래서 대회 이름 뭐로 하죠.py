9347e123d6b8287531db1dac520d587ec04fe6c5
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2025-09-07 02:28:45 +0900

    [Silver IV] Title: 그래서 대회 이름 뭐로 하죠, Time: 68 ms, Memory: 36604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.py
+++ 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.py
@@ -1,15 +1,42 @@
-import sys, itertools
+import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
-T = input()
-T = sorted(T, reverse=True)
+s = list(input())
+
 V = ['A', 'E', 'I', 'O', 'U']
-if T.count('A') < 2:
-    print('NO')
-else:
-    for i in itertools.permutations(T, M):
-        i = ''.join(i)
-        if i[-2] == 'A' and i[-3] == 'A' and i[-1] not in V:
-            print(i)
-            break
+ans = []
+
+while s and s[-1] in V:
+    s.pop()
+
+if not s:
+    print("NO")
+    sys.exit()
+
+ans.append(s.pop())
+
+while s and s[-1] != 'A':
+    s.pop()
+
+if not s:
+    print("NO")
+    sys.exit()
+
+ans.append(s.pop())
+
+while s and s[-1] != 'A':
+    s.pop()
+
+if not s:
+    print("NO")
+    sys.exit()
+
+ans.append(s.pop())
+
+if len(s) + 3 < M:
+    print("NO")
+    sys.exit()
+
+print("YES")
+print("".join(s[:M-3] + ans[::-1]))
