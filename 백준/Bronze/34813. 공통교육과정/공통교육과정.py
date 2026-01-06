5bd3e9975195cedeea3417eef897d9a750f66ac9
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2026-01-07 01:55:50 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,10 @@
+num = input()
+
+if num[0] == "F":
+    print("Foundation")
+elif num[0] == "C":
+    print("Claves")
+elif num[0] == "V":
+    print("Veritas")
+elif num[0] == "E":
+    print("Exploration")
