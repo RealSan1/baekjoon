a187a7ad7ff2b3c31c29364cb91ca4d105be115c
RealSan1 <127619099+RealSan1@users.noreply.github.com>
2026-01-26 01:01:20 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 732 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,11 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+a, b, c, d, e, f = map(int, input().split())
+A = set()
+B = set()
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a * x + b * y == c and d * x + e * y == f:
+            print(x, y)
+            exit()
